4b18fe1131d676249af14d666237db79c249bb0b
AnotherSamWithADream <106000727+AnotherSamWithADream@users.noreply.github.com>
2024-12-03 23:24:09 -0600

    Create IntakeCalibrationDashboard.java
    created easier method for calibrating intake through dashboard

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeCalibrationDashboard.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+@Config
+public class IntakeCalibrationDashboard extends LinearOpMode {
+    public static double clawpos = 0;
+    public static double anglerpos = 0;
+
+    @Override
+    public void runOpMode() {
+        Servo claw = hardwareMap.get(Servo.class, "claw");
+        Servo angler = hardwareMap.get(Servo.class, "angler");
+        DcMotorEx twinTowerMotor = hardwareMap.get(DcMotorEx.class, "twintower");
+
+        // Configure the motor to use the encoder for more precise control
+        twinTowerMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        twinTowerMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.right_stick_button || gamepad1.left_stick_button) {
+                angler.setPosition(anglerpos);
+                claw.setPosition(clawpos);
+            }
+
+            if (gamepad1.y) {
+                if (gamepad1.right_trigger > 0) {
+                    int targetPosition = twinTowerMotor.getCurrentPosition() + (int) (gamepad1.right_trigger * 1993.6); // Use encoder PPR value to calculate target
+                    twinTowerMotor.setTargetPosition(targetPosition);
+                    twinTowerMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    twinTowerMotor.setPower(1.0);
+                }
+                if (gamepad1.left_trigger > 0) {
+                    int targetPosition = twinTowerMotor.getCurrentPosition() - (int) (gamepad1.left_trigger * 1993.6); // Use encoder PPR value to calculate target
+                    twinTowerMotor.setTargetPosition(targetPosition);
+                    twinTowerMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    twinTowerMotor.setPower(1.0);
+                }
+            }
+            telemetry.addData("Current Angler Angle in Degrees", angler.getPosition() * 300);
+            telemetry.addData("Current Angler Position", angler.getPosition());
+            telemetry.addData("Current Claw Position", claw.getPosition());
+            telemetry.addData("Current Twin Tower Motor Power", twinTowerMotor.getPower());
+            telemetry.addData("Current Twin Tower Motor Position", twinTowerMotor.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+    }
+}
